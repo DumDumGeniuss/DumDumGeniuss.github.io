0854d84e885857c7c34bdbfc9209a9429bdccfaf
DumDumGeniuss <messi_yang@syntrontech.com>
2016-07-14 23:02:50 +0800

    fix some code

--- planet-revolution/index.js
+++ planet-revolution/index.js
@@ -7,10 +7,11 @@ var myEarthElement = document.getElementById('my-earth'),
 radius = 200;
 speed = 1;
 radian = 0;
+
 updateLocation = function() {
-	radian = radian - speed*0.1;
-	myEarthElement.style.left = "-webkit-calc(50% + " + radius*Math.cos(radian) + "px - 25px)";
-	myEarthElement.style.top = "-webkit-calc(50% + " + radius*Math.sin(radian) + "px - 25px)";
+	radian = radian + speed*0.1;
+	myEarthElement.style.left = "calc(50vw - 25px + " + radius*Math.cos(radian) + "px)";
+	myEarthElement.style.top = "calc(50vh - 25px - " + radius*Math.sin(radian) + "px)";
 	myEarthElement.style.transform = "rotate(" + radian*100 + "deg)";
 };
 
