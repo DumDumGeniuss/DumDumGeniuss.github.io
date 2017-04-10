ec3c6d41b0946cb00624ce7d8375c1c9f602005c
DumDumGeniuss <dumdumgenius@gmail.com>
2017-04-11 00:15:18 +0800

    Set home page

---
+++ home-page/webpack.config.js
@@ -0,0 +1,30 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  context: path.join(__dirname),
+  devtool: debug ? "inline-sourcemap" : null,
+  entry: "./src/client.js",
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
+        }
+      }
+    ]
+  },
+  output: {
+    path: path.join(__dirname),
+    filename: "./dist/client.min.js"
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
+};
