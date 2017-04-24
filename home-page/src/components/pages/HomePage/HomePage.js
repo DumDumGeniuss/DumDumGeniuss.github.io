495c1b5bacf8ca912221ceacbd7ed7a3ef9aaae7
DumDumGeniuss <dumdumgenius@gmail.com>
2017-04-24 13:06:12 +0800

    make my website great again

--- home-page/src/components/pages/HomePage/HomePage.js
+++ home-page/src/components/pages/HomePage/HomePage.js
@@ -5,6 +5,8 @@ import SocialIconsZone from '../../zones/SocialIconsZone/SocialIconsZone.js';
 import TravelMapZone from '../../zones/TravelMapZone/TravelMapZone.js';
 import BeepBeepZone from '../../zones/BeepBeepZone/BeepBeepZone.js';
 import MainLayout from '../../layouts/MainLayout/MainLayout.js';
+import ArrowUp from 'react-icons/lib/fa/arrow-up';
+import ArrowDown from 'react-icons/lib/fa/arrow-down';
 import style from './HomePage.css';
 
 class HomePage extends React.Component {
@@ -13,26 +15,43 @@ class HomePage extends React.Component {
 		this.state = {
 		};
 		this.currentLockPageY = 0;
-		this.tartgetLockPageY = 0;
+		this.lockZone = this.lockZone.bind(this);
 	}
 	componentDidMount() {
+		window.addEventListener("scroll", this.lockZone);
 	}
 	componentWillUnmount() {
 	}
-				// <GhostFontZone
-				// 	ref={'firstZone'}
-				// 	width={100}
-				// 	widthUnit={'%'}
-				// 	height={100}
-				// 	heightUnit={'vh'}
-				// 	backgroundColor={'#1f1f1f'}
-				// />
+	changeZone(isDown) {
+		const windowHeight = window.innerHeight;
+		const mainZoneHeight = ReactDOM.findDOMNode(this.refs.mainZone).clientHeight;
+
+		if (isDown) {
+			this.currentLockPageY += windowHeight;
+			if (this.currentLockPageY >= mainZoneHeight) {
+				this.currentLockPageY = 0;
+			}
+		} else {
+			this.currentLockPageY -= windowHeight;
+			if (this.currentLockPageY < 0) {
+				this.currentLockPageY = mainZoneHeight - windowHeight;
+			}
+		}
+		scrollTo(0, this.currentLockPageY);
+	}
+	lockZone () {
+		scrollTo(0, this.currentLockPageY);
+	}
 	render() {
 		return (
 			<MainLayout>
-				<div className={style.mainZone}>
+				<div ref={'mainZone'} className={style.mainZone}>
+					<div className={style.zoneControl}>
+						<ArrowUp onClick={this.changeZone.bind(this, false)} className={style.arrow}/>
+						<ArrowDown onClick={this.changeZone.bind(this, true)} className={style.arrow}/>
+					</div>
 					<BeepBeepZone
-						ref={'thirdZone'}
+						ref={'firstZone'}
 						width={100}
 						widthUnit={'%'}
 						height={100}
@@ -47,6 +66,14 @@ class HomePage extends React.Component {
 						height={100}
 						heightUnit={'vh'}
 						backgroundColor={'white'}
+					/>
+					<BeepBeepZone
+						ref={'thirdZone'}
+						width={100}
+						widthUnit={'%'}
+						height={100}
+						heightUnit={'vh'}
+						backgroundColor={'black'}
 					/>
 				</div>
 			</MainLayout>
