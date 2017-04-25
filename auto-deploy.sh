0b6c4a1a2984e0af45eb7357bd8a72bf44146706
DumDumGeniuss <dumdumgenius@gmail.com>
2017-04-26 01:10:34 +0800

    make my website great again

--- auto-deploy.sh
+++ auto-deploy.sh
@@ -2,7 +2,7 @@ git add .
 git commit -m "make my website great again"
 yarn deploy
 git checkout gh-pages
-git pull origin ph-pages
+git pull origin gh-pages
 git checkout master
 git merge gh-pages
 git push origin master
